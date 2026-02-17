503becad953085fb037386242000bce7a99ba0c6
Lakshay <lakshay2876@gmail.com>
2026-02-18 01:05:39 +0530

    relationships

--- DajangoBackend/myNewApp/admin.py
+++ DajangoBackend/myNewApp/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from . import models
+from .models import ChaiVariety, ChaiReviews
+
+
+class ChaiReviewsAdmin(admin.TabularInline) :
+    model = ChaiReviews
+    extra = 1
+class ChaiVarietyAdmin(admin.ModelAdmin) :
+    list_display = ('name', 'image', 'date_added', 'type', 'description')
+    inlines = [ChaiReviewsAdmin]
 
 # Register your models here.
-admin.site.register(models.ChaiVariety)
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
--- DajangoBackend/myNewApp/models.py
+++ DajangoBackend/myNewApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -15,6 +16,34 @@ class ChaiVariety(models.Model):
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES, default='ML')
     description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+    
+
+# one to many relationship
+class ChaiReviews(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.CharField(max_length=100)
+    review_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.name} left a rating and review of {self.chai.name}'
+    
+
+# Many to Many Relationship
+class ChaiStore(models.Model) :
+    chai_varities = models.ManyToManyField(ChaiVariety, related_name='stores')
+
+
+    def __str__(self):
+        return self.name
+    
+
+# One to One Relationship
+class ChaiCertificates(models.Model) :
+    certificate = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name='certificates')
 
     def __str__(self):
         return self.name
