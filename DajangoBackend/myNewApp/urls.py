f7ea6e63da776ad9618530efc09a9d38790dee5f
Lakshay <lakshay2876@gmail.com>
2026-02-17 19:36:57 +0530

    models added

--- DajangoBackend/myNewApp/urls.py
+++ DajangoBackend/myNewApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chai_order, name= 'ChaiAppPage'),
+    path('', views.chai_order, name= 'chai_order'),
+    path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
 ]
--- DajangoBackend/myNewApp/views.py
+++ DajangoBackend/myNewApp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ChaiVariety
 
 # Create your views here.
 def chai_order(request) :
-    return render(request,'myNewApp/app_all.html')
+    chais = ChaiVariety.objects.all()
+    return render(request,'myNewApp/app_all.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVariety, pk=chai_id)
+    return render(request, 'myNewApp/chai_detail.html', {'chai':chai})
